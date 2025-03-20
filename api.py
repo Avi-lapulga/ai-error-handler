c1bbfb7af7cfb3f6fe709a38f36bf06063be4616
S.AVINASH <avinashswims@gmail.com>
2025-03-20 08:43:35 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from llama_cpp import Llama
 
 # Initialize the Llama model
-llama = Llama(model_path=r"C:\Users\unela\OneDrive\Desktop\ai_error_handler\ai_error_handler\meta-llama-3-8b_q8_0.gguf")
+llama = Llama(model_path=r"C:\Users\unela\OneDrive\Desktop\ai_error_handler\meta-llama-3-8b_q8_0.gguf")
 
 def get_ai_suggestions(error_message: str):
     # Use Llama to generate a suggestion for an error message
